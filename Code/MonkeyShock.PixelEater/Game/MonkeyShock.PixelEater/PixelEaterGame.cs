55ad274d08c154919d3c554be8165024ed8fcea5
Piotr Gaszewski <piotrek.gaszewski@hotmail.com>
2018-05-29 09:05:04 +0200

    Program icon changes and Game1 class name changed into PixelEaterGame

--- Code/MonkeyShock.PixelEater/Game/MonkeyShock.PixelEater/PixelEaterGame.cs
+++ Code/MonkeyShock.PixelEater/Game/MonkeyShock.PixelEater/PixelEaterGame.cs
@@ -7,12 +7,12 @@ namespace MonkeyShock.PixelEater
     /// <summary>
     /// This is the main type for your game.
     /// </summary>
-    public class Game1 : Game
+    public class PixelEaterGame : Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
-        public Game1()
+        public PixelEaterGame()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
--- Code/MonkeyShock.PixelEater/Game/MonkeyShock.PixelEater/Program.cs
+++ Code/MonkeyShock.PixelEater/Game/MonkeyShock.PixelEater/Program.cs
@@ -14,7 +14,7 @@ namespace MonkeyShock.PixelEater
         [STAThread]
         static void Main()
         {
-            using (var game = new Game1())
+            using (var game = new PixelEaterGame())
                 game.Run();
         }
     }
