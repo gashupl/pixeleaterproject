e33bbdd4b92bb9f806b3256e39c27189fed2d0a7
Piotr Gaszewski <piotrek.gaszewski@hotmail.com>
2018-06-05 19:42:46 +0200

    First logo letter :)

---
+++ Code/MonkeyShock.PixelEater/Game/MonkeyShock.PixelEater/Common/ColorDataFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonkeyShock.PixelEater.Common
+{
+    public class ColorDataFactory
+    {
+        public Color[] Get(int colorArraySize, Color color)
+        {
+            Color[] colorData = new Color[colorArraySize];
+            for (int i = 0; i < colorArraySize; i++)
+            {
+                colorData[i] = color;
+            }
+            return colorData; 
+        }
+    }
+}
--- Code/MonkeyShock.PixelEater/Game/MonkeyShock.PixelEater/Common/WelcomeScreen.cs
+++ Code/MonkeyShock.PixelEater/Game/MonkeyShock.PixelEater/Common/WelcomeScreen.cs
@@ -8,23 +8,25 @@ namespace MonkeyShock.PixelEater.Common
     class WelcomeScreen : GameStateBase
     {
         private Vector2 initialPosition = new Vector2(50,50);
-        private Texture2D squareTexture;
-        private int squareTextureSize = 30;
+        private Texture2D[] titleTextures;
+        private int singleSquareTextureSize = 30;
         private Texture2D[] textures;
 
         public override void Initialize(GraphicsDevice graphicsDevice)
         {
             this.graphicsDevice = graphicsDevice;
-            this.squareTexture = new Texture2D(this.graphicsDevice, squareTextureSize, squareTextureSize * 6);
+            this.titleTextures = new Texture2D[4]; 
+            this.titleTextures[0] = new Texture2D(this.graphicsDevice, singleSquareTextureSize, singleSquareTextureSize * 6);
+            this.titleTextures[1] = new Texture2D(this.graphicsDevice, singleSquareTextureSize * 3, singleSquareTextureSize);
+            this.titleTextures[2] = new Texture2D(this.graphicsDevice, singleSquareTextureSize * 3, singleSquareTextureSize);
+            this.titleTextures[3] = new Texture2D(this.graphicsDevice, singleSquareTextureSize, singleSquareTextureSize * 2);
 
-            var colorArraySize = squareTextureSize * squareTextureSize * 6;
-            Color[] colorData = new Color[colorArraySize];
-            for (int i = 0; i < colorArraySize; i++)
-            {
-                colorData[i] = Color.Yellow;
-            }
+            var colorDataFactory = new ColorDataFactory(); 
 
-            this.squareTexture.SetData<Color>(colorData);
+            this.titleTextures[0].SetData<Color>(colorDataFactory.Get(singleSquareTextureSize * singleSquareTextureSize * 6, Color.Yellow));
+            this.titleTextures[1].SetData<Color>(colorDataFactory.Get(singleSquareTextureSize * 3 * singleSquareTextureSize, Color.Yellow));
+            this.titleTextures[2].SetData<Color>(colorDataFactory.Get(singleSquareTextureSize * 3 * singleSquareTextureSize, Color.Yellow));
+            this.titleTextures[3].SetData<Color>(colorDataFactory.Get(singleSquareTextureSize * singleSquareTextureSize * 2, Color.Yellow));
         }
 
         public override void HandleKeyboardEvents()
@@ -38,7 +40,11 @@ namespace MonkeyShock.PixelEater.Common
         public override void Draw(SpriteBatch spriteBatch)
         {
             spriteBatch.Begin();
-            spriteBatch.Draw(squareTexture, initialPosition, Color.Yellow);
+            spriteBatch.Draw(this.titleTextures[0], initialPosition, Color.Yellow);
+            spriteBatch.Draw(this.titleTextures[1], new Vector2(initialPosition.X + singleSquareTextureSize, initialPosition.Y), Color.Yellow);
+            spriteBatch.Draw(this.titleTextures[2], new Vector2(initialPosition.X + singleSquareTextureSize, initialPosition.Y + (singleSquareTextureSize * 3)), Color.Yellow);
+            spriteBatch.Draw(this.titleTextures[3], new Vector2(initialPosition.X + (singleSquareTextureSize * 3), initialPosition.Y + singleSquareTextureSize), Color.Yellow);
+            
             spriteBatch.End();
         }
 
