b2fb944249b673e140e560fef471ed3a8656a003
Piotr Gaszewski <piotrek.gaszewski@hotmail.com>
2018-07-12 20:55:17 +0200

    GameScreenActionInvoker class implemented

--- Code/MonkeyShock.PixelEater/Game/MonkeyShock.Common/TimeCounter.cs
+++ Code/MonkeyShock.PixelEater/Game/MonkeyShock.Common/TimeCounter.cs
@@ -9,13 +9,17 @@ namespace MonkeyShock.Common
 {
     public class TimeCounter
     {
+
         private int savedTotalGameTime = 0; // seconds
-        private Action onTimesUpAction; 
+        private Action onTimesUpAction;
+        private List<Action> periodicActions; 
 
         public int RemainingTime { get; private set; }
+        public int InitialTime { get; private set; }
 
         public TimeCounter(int intialSecondsNumber, Action onTimesUpAction)
         {
+            this.InitialTime = intialSecondsNumber; 
             this.RemainingTime = intialSecondsNumber;
             this.onTimesUpAction = onTimesUpAction; 
         }
---
+++ Code/MonkeyShock.PixelEater/Game/MonkeyShock.PixelEater/Helpers/GameScreenActionInvoker.cs
@@ -0,0 +1,42 @@
+﻿using MonkeyShock.Common;
+using MonkeyShock.PixelEater.Objects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonkeyShock.PixelEater.Helpers
+{
+    class GameScreenActionInvoker
+    {
+        private Action<List<Food>> generateFoodAction;
+        private List<Food> food; 
+        private TimeCounter timeCounter;
+        private int lastRemainingTime;
+        private int invokeFrequencySeconds; 
+
+        public GameScreenActionInvoker(TimeCounter timeCounter)
+        {
+            this.timeCounter = timeCounter;
+            this.lastRemainingTime = this.timeCounter.InitialTime; 
+        }
+
+        public void AddGenerateFoodAction(Action<List<Food>> generateFoodAction, List<Food> food, int invokeFrequencySeconds)
+        {
+            this.generateFoodAction = generateFoodAction;
+            this.food = food; 
+            this.invokeFrequencySeconds = invokeFrequencySeconds; 
+        }
+
+        public void Update()
+        {
+            var timeDifference = lastRemainingTime - timeCounter.RemainingTime; 
+            if(timeDifference == invokeFrequencySeconds)
+            {
+                this.generateFoodAction.Invoke(food);
+                lastRemainingTime = timeCounter.RemainingTime; 
+            }
+        }
+    }
+}
