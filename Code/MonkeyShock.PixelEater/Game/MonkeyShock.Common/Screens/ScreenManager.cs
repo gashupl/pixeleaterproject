33326e5e7aedaf607d34e77d4e1c9dcb1e688356
Piotr Gaszewski <piotrek.gaszewski@hotmail.com>
2018-07-16 20:32:22 +0200

    ScreenManager and abstract GameScreen classes added

---
+++ Code/MonkeyShock.PixelEater/Game/MonkeyShock.Common/Screens/ScreenManager.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonkeyShock.Common.Screens
+{
+    public class ScreenManager : DrawableGameComponent
+    {
+        #region Private fields
+        private List<GameScreen> gameScreens = new List<GameScreen>();
+        private List<GameScreen> screensToUpdate = new List<GameScreen>();
+        #endregion
+
+        #region Public properties
+        new public Game Game
+        {
+            get
+            {
+                return base.Game; 
+            }
+        }
+        #endregion
+
+        #region Constructor
+        public ScreenManager(Game game, GraphicsDeviceManager graphicsDeviceManager): base(game)
+        {
+
+        }
+        #endregion
+
+    }
+}
